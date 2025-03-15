7f8f3417cb648127b660ff3dbf719c2c9a97032b
AbdullahSharaawy <162289302+AbdullahSharaawy@users.noreply.github.com>
2025-03-15 09:15:59 +0200

    Create DataAccess.h

---
+++ DataAccess.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<limits>
+#include"DataInput.h"
+using namespace std;
+template<typename T>
+class  DataAccess
+{
+
+public:
+    static void ReadData(T* & _Data,int & size);// read data from the file
+   
+};
+
+
+
+template<typename T>
+inline void DataAccess<T>::ReadData(T* & _Data,int & size)
+{
+    
+    string FileName = DataInput::ReadFileName("Enter The File Name: ");
+    string line;
+    int index = 0;
+    
+    ifstream file(FileName);
+      
+        while (getline(file, line))
+            size++;
+        // Reset the file pointer to the beginning
+        file.clear();
+        file.seekg(0, ios::beg);
+
+    _Data = new T[size];
+    
+    while ( index < size) {
+        file>> _Data[index++];
+    }
+   
+    file.close();
+    
+}
+
+
